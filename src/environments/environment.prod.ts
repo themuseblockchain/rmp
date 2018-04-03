351b99474b06ade0ab4891b5b0cfaee72a1ccfae
jfortner <johnpfortner@gmail.com>
2018-04-03 10:43:09 -0500

    Adding Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  googleApi: ''
+  googleApi: '',
+    firebase: {
+            apiKey: 'AIzaSyDUyOeMNp1nUSBX3JpCjwF1G4ujk-Z6EbU',
+            authDomain: 'rights-management-portal.firebaseapp.com',
+            databaseURL: 'https://rights-management-portal.firebaseio.com',
+            projectId: 'rights-management-portal',
+            storageBucket: 'rights-management-portal.appspot.com',
+            messagingSenderId: '768176307619'
+        }
 };
